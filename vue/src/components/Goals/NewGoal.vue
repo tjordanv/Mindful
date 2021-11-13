9a0ad7f9b3d4f3265ab0c25d38f40a65df719adf
tjordanv <tylervicari@gmail.com>
2021-11-13 15:57:50 -0500

    new goal form style and layout
    began working on new goal form and view. broke the form into three tabs and added progression balls at the bottom

--- vue/src/components/Goals/NewGoal.vue
+++ vue/src/components/Goals/NewGoal.vue
@@ -1,30 +1,54 @@
 <template>
-  <div>
-      <form class="newGoal" @submit.prevent="createNewGoal">
-        <input type="text" class="newGoalInput" placeholder="Summary" v-model="newGoal.summary" required>
-        <textarea class="descriptionInput" placeholder="Description" cols="30" rows="3" v-model="newGoal.description" required></textarea> 
-        <label for="units">Units</label>
-        <select name="units" v-model="newGoal.units" required>
-            <option value="dollars">Dollars</option>
-            <option value="lbs">Pounds</option>
-            <option value="time">Time</option>
-        </select>  
-        <label for="movement">Movement</label>
-        <select name="movement" v-model="newGoal.movement" required>
-            <option value="total up">Total Up</option>
-            <option value="total down">Total Down</option>
-            <option value="average up">Average Up</option>
-            <option value="average down">Average Down</option>
-        </select> 
-         <!--add v-ifs to display the correct goal input field that corresponds with the units  -->
-        <input type="number" class="goalCurrency" placeholder="Goal" v-model="newGoal.goal" v-if="newGoal.units === 'dollars'"  min="0" required>  
-        <input type="text" class="goal" placeholder="Goal" v-model="newGoal.goal" v-if="newGoal.units !== 'dollars'" required>  
-        <label for="startDate">Start Date</label>
-        <input type="Date" class="newGoalDateInput" name="startDate" v-model="newGoal.startDate" required>  
-        <label for="endDate">End Date</label>
-        <input type="Date" class="newGoalDateInput" name="endDate" v-model="newGoal.endDate" required>  
-        <button type="submit">click me</button>
-        <button class="back-btn" @click.prevent="navToGoals">Back</button>
+  <div class="container">
+      <form class="newGoal">
+        <div class="tab" v-show="currentTab == 0">
+            <p>Provide a brief summary of your goal, this should be a concise call to action. 
+                You can also think of this as a title or header!</p>
+            <p><input type="text" class="input" placeholder="Summary" maxlength="25"
+            v-model="newGoal.summary" oninput="this.className = 'newGoalSum, input'"></p>
+            <p>Take a moment to think a bit deeper about this goal and then provide a more detailed 
+                description below. Why are you persuing this goal and what impact will it have on 
+                you? How will you reach this goal and hold yourself accountable along the way? Thoughts
+                like these must be considered in order to set effective goals.  
+            </p>
+            <p><textarea class="input" placeholder="Description" rows="5" maxlength="800"
+            v-model="newGoal.description" oninput="this.className = 'input'"></textarea></p> 
+        </div>
+        <div class="tab" v-show="currentTab == 1">
+            <label for="units">Units</label>
+            <select class="input" name="units" v-model="newGoal.units" oninput="this.className = 'input'">
+                <option value="dollars">Dollars</option>
+                <option value="lbs">Pounds</option>
+                <option value="time">Time</option>
+            </select>  
+            <label for="movement">Movement</label>
+            <select class="input" name="movement" v-model="newGoal.movement" oninput="this.className = 'input'">
+                <option value="total up">Total Up</option>
+                <option value="total down">Total Down</option>
+                <option value="average up">Average Up</option>
+                <option value="average down">Average Down</option>
+            </select> 
+            <!--add v-ifs to display the correct goal input field that corresponds with the units  -->
+            <input type="number" class="goalCurrency, input" placeholder="Goal" v-model="newGoal.goal" v-if="newGoal.units === 'dollars'"  min="0" oninput="this.className = 'goalCurrecny, input'">  
+            <input type="text" class="goal, input" placeholder="Goal" v-model="newGoal.goal" v-if="newGoal.units !== 'dollars'" oninput="this.className = 'goal, input'">  
+        </div>  
+        <div class="tab" v-show="currentTab == 2">
+            <label for="startDate">Start Date</label>
+            <input type="Date" class="newGoalDateInput, input" name="startDate" v-model="newGoal.startDate" oninput="this.className = 'newGoalDate, input'">  
+            <label for="endDate">End Date</label>
+            <input type="Date" class="newGoalDateInput, input" name="endDate" v-model="newGoal.endDate" oninput="this.className = 'newGoalDate, input'">  
+        </div>
+        <div>
+            <button class="NextBtn" @click.prevent="tabNav(1)" v-if="currentTab != 2">Next</button>
+            <button class="submitBtn" @click.prevent="createNewGoal()" v-if="currentTab == 2">Save</button>
+            <button class="BackBtn" @click.prevent="tabNav(-1)" v-if="currentTab != 0">Back</button>
+            <button class="CancelBtn" @click.prevent="navToGoals" v-if="currentTab == 0">Cancel</button>
+        </div>
+        <div class="progressTracker">
+            <span class="step"></span>
+            <span class="step"></span>
+            <span class="step"></span>
+        </div>
       </form>
   </div>
 </template>
@@ -48,6 +72,7 @@ export default {
                 active: ""
             },
             currentDate: "",
+            currentTab: 0,
         }
     },
     created() {
@@ -65,12 +90,17 @@ export default {
         if (this.$store.state.currentDate === "") {
             this.$store.commit("SET_CURRENT_DATE");
         }
-        this.currentDate = this.$store.state.currentDate; 
+        
     },
+    mounted() {
+        let steps = document.getElementsByClassName("progressTracker");
+        steps[0].firstElementChild.classList.add("active");
+    } ,
     methods: {
         createNewGoal() {
-            if (this.newGoal.startDate > this.currentDate) {
-                this.newGoal.active = false;
+            if (this.validateForm()) {
+                if (this.newGoal.startDate > this.currentDate) {
+                    this.newGoal.active = false;
             } else if (this.newGoal.startDate <= this.currentDate) {
                 this.newGoal.active = true;
             }
@@ -79,14 +109,94 @@ export default {
                 this.$store.commit("INCREMENT_ACTIVE_GOALS"),
                 this.$router.push("/goals")
             )
+        }
         }, 
         navToGoals() {
             this.$router.push("/goals");
+        },
+        tabNav(n) {
+            if (n == 1 && !this.validateForm()) {
+                return false;
+            } else {
+                this.currentTab += n;
+                this.stepIndicator(this.currentTab);
+            }
+        },
+        validateForm() {
+            let valid = true;
+            let tabs = document.getElementsByClassName("tab");
+            let tabInputs = tabs[this.currentTab].getElementsByClassName("input");
+            for (let i = 0; i < tabInputs.length; i ++) {
+                if (tabInputs[i].value == "") {
+                    tabInputs[i].classList.add("invalid");
+                    valid = false;
+                } 
+            }
+
+            if (valid) {
+                document.getElementsByClassName("step")[this.currentTab].className += " complete";
+            }
+
+            return valid;
+        },
+        stepIndicator(n) {
+            let steps = document.getElementsByClassName("step");
+            for (let i = 0; i < steps.length; i++) {
+                steps[i].classList.remove("active");
+                // steps[i].className = steps[i].className.replace(" active", "");
+            }
+            steps[n].classList.add("active");
         }
     },
 }
 </script>
 
-<style>
-
+<style scoped>
+.container {
+display: grid;
+  grid-template-columns: 1fr;
+  grid-template-areas: "newGoal";
+  width: 100%;
+  height: 100vh;
+  background-color: #eff2f1;
+}
+.newGoal {
+    width: 400px;
+    margin-top: 5%;
+    grid-area: newGoal;
+    justify-self: center;
+    background-color: #f4b942;
+}
+input[type=text] {
+    width: 90%;
+}
+textarea {
+    width: 90%;
+}
+.invalid {
+    background-color: #ffdddd;
+}
+p {
+    text-align: center;
+}
+.progressTracker {
+    text-align: center;
+    margin-top: 40px;
+}
+.step {
+    height: 20px;
+    width: 20px;
+    margin: 0 3px;
+    background-color: white;
+    border: none;
+    border-radius: 50%;
+    display: inline-block;
+    opacity: 0.5;
+}
+.active {
+    opacity: 1;
+}
+.step.complete {
+    background-color: green;
+}
 </style>
--- vue/src/views/Goals/NewGoal.vue
+++ vue/src/views/Goals/NewGoal.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+      <GoalsHeader></GoalsHeader>
       <NewGoal></NewGoal>
   </div>
 </template>
 
 <script>
 import NewGoal from "../../components/Goals/NewGoal.vue";
+import GoalsHeader from "../../components/Goals/GoalsHeader.vue";
 
 export default {
-    components: { NewGoal },
+    components: { NewGoal, GoalsHeader },
 
 }
 </script>
